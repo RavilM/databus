5953cdf4c634a425e05e71eabc7cdafafbc350d0
Ravil Mamashev <ravil.mamashev@wildberries.ru>
2020-09-01 16:01:44 +0300

    Remove artifacts

--- lib/components/subscriber.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export declare function subscriber<T = any>({ getStateToProps, }: {
-    getStateToProps: Array<string>;
-}): (WrappedComponent: React.ComponentType<T>) => React.ComponentType<T>;
